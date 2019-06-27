73f6505ad756771fe2dec92be55cb7c7537e4eb7
jota <jota@criptext.com>
2019-06-26 22:32:03 -0300

    Configurando estrutura Nodemon & Sucrase Conceitos do Docker Configurando Docker Sequelize & MVC ESLint, Prettier & EditorConfig Configurando Sequelize Migration de usuário Model de usuário Criando loader de models Cadastro de usuários Gerando hash da senha Conceitos de JWT Autenticação JWT Middleware de autenticação Update do usuário

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import UserController from './app/contollers/UserController';
+import SessionController from './app/contollers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
