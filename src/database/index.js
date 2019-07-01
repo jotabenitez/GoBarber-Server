cafde8745328bb79ad7d44b1faa1e3b1de6af6cc
jota <jota@criptext.com>
2019-07-01 13:42:15 -0300

    apointments db

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,29 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
+    this.connection = new Sequelize(databaseConfig);
+
     this.init();
+    this.associate();
   }
 
   init() {
-    this.connection = new Sequelize(databaseConfig);
-
     models.map(model => model.init(this.connection));
+  }
+
+  associate() {
+    models.map(
+      model => model.associate && model.associate(this.connection.models)
+    );
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,31 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/contollers/UserController';
 import SessionController from './app/contollers/SessionController';
+import FileController from './app/contollers/FileController';
+import ProviderController from './app/contollers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// Rotas
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+// rotas do curso
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+// rotas adiciocadas p mim
 routes.get('/users', UserController.show);
 routes.delete('/users', UserController.delete);
 
